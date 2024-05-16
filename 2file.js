ab4f3161e79c7e5c670f88ec2f94325c5c4ed1a7
sumanthravuri <91436400+sumanthravuri@users.noreply.github.com>
2024-05-16 15:35:35 +0530

    Create 2file.js

---
+++ 2file.js
@@ -0,0 +1,13 @@
+function print(name){
+    console.log(name);
+}
+module.exports.fun=print;
+const data=['wse','wedfs','trfvgwseardf','ftyfyt','gvtgyg']
+module.exports.data=data;
+const person={
+    "name":"sumanth",
+    "id":"21b81a33b5",
+    "section":"b",
+    "branch":"csit"
+}
+module.exports.person=person
